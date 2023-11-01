7f76fea7b9940896af17997338c2820c8a724627
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-01 15:20:28 +0100

    create: first test of typescript

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+"use strict";
+var emunVar;
+(function (emunVar) {
+    emunVar[emunVar["One"] = 0] = "One";
+    emunVar[emunVar["Two"] = 1] = "Two";
+    emunVar[emunVar["Three"] = 2] = "Three";
+})(emunVar || (emunVar = {}));
